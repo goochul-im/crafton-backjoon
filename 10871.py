e3cda8d7c4fb4067338681843f9708cb7cf10b39
goochul-im <goochul175465@gmail.com>
2025-03-14 12:11:22 +0900

    10871

--- 10871.py
+++ 10871.py
@@ -4,4 +4,4 @@ n = int(sys.stdin.readline().split()[1])
 prob = map(int, sys.stdin.readline().split())
 
 prob = [x for x in prob if x < n]
-print(" ".join(map(str, prob)))
+print(" ".join(map(str, prob))) # join은 숫자 리스트에서는 적용되지 않으므로 문자열로 바꿔야 함
