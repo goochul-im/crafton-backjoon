3c01e20639ef0027224ab090e170fe4081fdee1d
goochul-im <goochul175465@gmail.com>
2025-03-14 10:43:13 +0900

    2903

--- 2903.py
+++ 2903.py
@@ -1,16 +1,15 @@
-# 2,
-# 2+1,
-# 2+1+2
-# 2+1+2+4
-# 2+1+2+4+8
-# 2+1+2+4+8+16 = 33
-# 2+1+2+4+8+16+32
+import sys
 
-# 33*33 = 1089
+number = int(sys.stdin.readline())
+
 
 def solution(n):
     point = 2
-    for i in range(1, n+1):
-        print(i)
+    adder = 1
+    for i in range(1, n + 1):
+        point += adder
+        adder *= 2
+    print(point ** 2)
 
-solution(5)
+
+solution(number)
