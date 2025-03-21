ade9cf0045ffa5cd13d36fa33a231e1f3aeeb70a
im_9fe <goochul175465@gmail.com>
2025-03-22 00:54:30 +0900

    [Gold V] Title: 괄호의 값, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.py
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.py
@@ -1,51 +1,49 @@
+# 2504 괄호의 값
 import sys
 
 prob = sys.stdin.readline().strip()
+
 stack = []
 
 
 def find():
     for char in prob:
-        if char == '(' or char == '[':  # 여는 괄호는 그냥 추가
+        if char == '(' or char == '[':  # 얘들은 그냥 추가
             stack.append(char)
-        elif char == ')':  # ')' 처리를 위한 로직
-            if not stack:  # 🚨 스택이 비어 있으면 에러 방지
+        elif char == ')':  # ')' 를 넣어야 할 때
+            if not stack:
                 print(0)
                 return
-            if isinstance(stack[-1], int):  # 숫자가 맨 위에 있으면
-                num = stack.pop()
-                if not stack or stack.pop() != '(':  # 🚨 pop() 전에 스택 체크
+            if isinstance(stack[-1], int):  # 숫자랑 만났을 때
+                num = stack.pop()  # 숫자 먼저 꺼내고
+                if not stack or stack.pop() != '(':  # 올바른 괄호가 아니면
                     print(0)
                     return
                 stack.append(num * 2)
-            else:
-                if stack.pop() != '(':  # 🚨 pop() 전에 스택 체크
+            else:  # 괄호랑 만나면
+                if stack.pop() != '(':  # 올바른 괄호가 아니면
                     print(0)
                     return
                 stack.append(2)
-
             if len(stack) > 1 and isinstance(stack[-2], int):
                 stack.append(stack.pop() + stack.pop())
-
-        elif char == ']':  # ']' 처리를 위한 로직
-            if not stack:  # 🚨 스택이 비어 있으면 에러 방지
+        else:  # ']' 를 넣어야 할 때
+            if not stack:
                 print(0)
                 return
-            if isinstance(stack[-1], int):  # 숫자가 맨 위에 있으면
-                num = stack.pop()
-                if not stack or stack.pop() != '[':  # 🚨 pop() 전에 스택 체크
+            if isinstance(stack[-1], int):  # 숫자랑 만났을 때
+                num = stack.pop()  # 숫자 먼저 꺼내고
+                if not stack or stack.pop() != '[':  # 올바른 괄호가 아니면
                     print(0)
                     return
                 stack.append(num * 3)
-            else:
-                if stack.pop() != '[':  # 🚨 pop() 전에 스택 체크
+            else:  # 괄호랑 만나면
+                if stack.pop() != '[':  # 올바른 괄호가 아니면
                     print(0)
                     return
                 stack.append(3)
-
             if len(stack) > 1 and isinstance(stack[-2], int):
                 stack.append(stack.pop() + stack.pop())
-
     if len(stack) == 1 and isinstance(stack[0], int):  # 정상적인 경우
         print(stack[0])
     else:
