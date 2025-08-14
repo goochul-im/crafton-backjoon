b6c1b75faec40f4d688583d510cd374f90da32f2
im_9fe <goochul175465@gmail.com>
2025-08-14 16:47:48 +0900

    [Bronze V] Title: 구구단, Time: 116 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+//  구구단
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= 9; i++) {
+            System.out.printf("%d * %d = %d\n", n, i, n*i);
+        }
+    }
+}
