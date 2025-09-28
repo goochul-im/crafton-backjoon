75ad78c769ec44b1fc88cd136c1f4deaa785117f
im_9fe <goochul175465@gmail.com>
2025-09-28 15:21:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 184 ms, Memory: 16400 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -0,0 +1,43 @@
+//  인간-컴퓨터 상호작용
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        char[] prob = br.readLine().toCharArray();
+        String[] line = br.readLine().split(" ");
+
+        int q = Integer.parseInt(line[0]);
+        int[][] arr = new int[prob.length][26];
+
+        arr[0][prob[0] - 97]++;
+
+        for (int i = 1; i < prob.length; i++) {
+            int now = prob[i] - 97;
+
+            arr[i][now]++;
+            for (int j = 0; j < 26; j++) {
+                arr[i][j] += arr[i - 1][j];
+            }
+        }
+
+        for (int i = 0; i < q; i++) {
+            line = br.readLine().split(" ");
+
+            int find = line[0].charAt(0) - 97;
+            int start = Integer.parseInt(line[1]);
+            int end = Integer.parseInt(line[2]);
+
+            int answer = arr[end][find];
+            if (start > 0) {
+                answer -= arr[start - 1][find];
+            }
+
+            System.out.println(answer);
+        }
+    }
+}
