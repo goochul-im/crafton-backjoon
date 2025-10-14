e7228508889d1cc40cde7e16c6ad57d8ca161279
im_9fe <goochul175465@gmail.com>
2025-10-14 18:54:52 +0900

    [Gold I] Title: 화살표 미로 (Normal), Time: 3264 ms, Memory: 414160 KB -BaekjoonHub

--- 백준/Gold/20420. 화살표 미로 （Normal）/화살표 미로 （Normal）.kt
+++ 백준/Gold/20420. 화살표 미로 （Normal）/화살표 미로 （Normal）.kt
@@ -2,23 +2,28 @@ import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.util.LinkedList
 import java.util.Queue
+import kotlin.math.min
 
-fun main() {
+//  화살표 미로 (Normal)
+
+fun main(args: Array<String>) {
+
+    var answer = "NO"
     val br = BufferedReader(InputStreamReader(System.`in`))
     val (ROW, COL, K) = br.readLine().split(" ").map { it.toInt() }
-    val map = Array(ROW) { br.readLine() }
-    
-    val isVisited = Array(ROW) { Array(COL) { Array(K + 1) { BooleanArray(K + 1) } } }
-    data class Point(val row: Int, val col: Int, val left: Int, val right: Int)
 
-    // BFS 함수를 main 함수 밖으로 빼거나, 아래 변수들을 bfs 내부로 옮겨야 합니다.
-    // 여기서는 bfs 내부로 옮기는 것으로 수정합니다.
-    fun bfs(): String {
+    val prob = Array(ROW) { br.readLine() }
+    val isVisited = Array(ROW) { Array(COL) { Array(K + 1) { BooleanArray(K + 1) } } }
+
+    data class Point(val row: Int, val col: Int, val leftScroll: Int, val rightScroll: Int)
+
+    fun bfs() : String {
         val q: Queue<Point> = LinkedList()
         q.add(Point(0, 0, K, K))
         isVisited[0][0][K][K] = true
 
-        val dx = intArrayOf(-1, 0, 1, 0) // 상, 우, 하, 좌
+        // 방향 배열과 맵핑 (상, 우, 하, 좌)
+        val dx = intArrayOf(-1, 0, 1, 0)
         val dy = intArrayOf(0, 1, 0, -1)
         val directions = mapOf('U' to 0, 'R' to 1, 'D' to 2, 'L' to 3)
 
@@ -29,47 +34,47 @@ fun main() {
                 return "Yes"
             }
 
-            val currentDir = directions[map[row][col]]!!
+            val currentDir = directions[prob[row][col]]!!
+            // case 1 : 그대로
+            val nextRow = row + dx[currentDir]
+            val nextCol = col + dy[currentDir]
 
-            // Case 1: 스크롤 미사용 (직진)
-            val nextRow0 = row + dx[currentDir]
-            val nextCol0 = col + dy[currentDir]
-            if (nextRow0 in 0..<ROW && nextCol0 in 0..<COL && !isVisited[nextRow0][nextCol0][left][right]) {
-                isVisited[nextRow0][nextCol0][left][right] = true
-                q.add(Point(nextRow0, nextCol0, left, right))
+            if (nextRow in 0..<ROW && nextCol in 0..<COL && !isVisited[nextRow][nextCol][left][right]) {
+                isVisited[nextRow][nextCol][left][right] = true
+                q.add(Point(nextRow, nextCol, left, right))
             }
 
-            // Case 2: 오른쪽 스크롤 사용 (1~3개)
-            var nextRightDir = currentDir
-            for (i in 1..3) {
-                if (right < i) break // 사용할 스크롤이 부족하면 중단
-                
-                nextRightDir = (nextRightDir + 1) % 4 // 90도씩 회전
-                val nextRow = row + dx[nextRightDir]
-                val nextCol = col + dy[nextRightDir]
-                val nextRight = right - i
+            // case 2 : 왼쪽 스크롤
+            for (scroll in 1..3) {
+                if (scroll > left) break
 
-                if (nextRow in 0..<ROW && nextCol in 0..<COL && !isVisited[nextRow][nextCol][left][nextRight]) {
-                    isVisited[nextRow][nextCol][left][nextRight] = true
-                    q.add(Point(nextRow, nextCol, left, nextRight))
+                val nextDir = ((currentDir - scroll) + 4) % 4
+                val nextRow = row + dx[nextDir]
+                val nextCol = col + dy[nextDir]
+                val curScroll = left - scroll
+
+                if (nextRow in 0..<ROW && nextCol in 0..<COL && !isVisited[nextRow][nextCol][curScroll][right]) {
+                    isVisited[nextRow][nextCol][curScroll][right] = true
+                    q.add(Point(nextRow, nextCol, curScroll, right))
                 }
             }
 
-            // Case 3: 왼쪽 스크롤 사용 (1~3개)
-            var nextLeftDir = currentDir
-            for (i in 1..3) {
-                if (left < i) break // 사용할 스크롤이 부족하면 중단
-                
-                nextLeftDir = (nextLeftDir - 1 + 4) % 4 // 90도씩 회전
-                val nextRow = row + dx[nextLeftDir]
-                val nextCol = col + dy[nextLeftDir]
-                val nextLeft = left - i
-                
-                if (nextRow in 0..<ROW && nextCol in 0..<COL && !isVisited[nextRow][nextCol][nextLeft][right]) {
-                    isVisited[nextRow][nextCol][nextLeft][right] = true
-                    q.add(Point(nextRow, nextCol, nextLeft, right))
+            // case 3 : 오른쪽 스크롤
+            for (scroll in 1..3) {
+                if (scroll > right) break
+
+                val nextDir = (currentDir + scroll) % 4
+                val nextRow = row + dx[nextDir]
+                val nextCol = col + dy[nextDir]
+                val curScroll = right - scroll
+
+                if (nextRow in 0..<ROW && nextCol in 0..<COL && !isVisited[nextRow][nextCol][left][curScroll]) {
+                    isVisited[nextRow][nextCol][left][curScroll] = true
+                    q.add(Point(nextRow, nextCol, left, curScroll))
                 }
+
             }
+
         }
         return "No"
     }
